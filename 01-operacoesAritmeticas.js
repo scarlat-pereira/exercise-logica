d5a517e6b5341568fed440fdcb8b7e93f9482365
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-24 13:42:13 -0300

    feat: exercise 1 - operacoes aritmeticas

---
+++ 01-operacoesAritmeticas.js
@@ -0,0 +1,12 @@
+const num1 = 2;
+const num2 = 4;
+
+const soma = num1 + num2;
+const subtracao = num1 - num2;
+const divisao = num1 / num2;
+const multiplicacao = num1 * num2;
+
+console.log(`Soma: 2 + 4 = ${soma}`);
+console.log(`Subtração: 2 - 4 = ${subtracao}`);
+console.log(`Divisão: 2 / 4 = ${divisao}`);
+console.log(`Multiplicação: 2 * 4 = ${multiplicacao}`);
